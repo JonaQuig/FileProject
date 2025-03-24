ccb4ae7e74ecc6c21488fa19c78cc6f6ade63e45
jonaquig <jonaquig@woodland.on.ca>
2025-03-24 12:13:35 -0400

    s

--- GUI.py
+++ GUI.py
@@ -9,6 +9,7 @@ window.title("File: Movie Field")
 name_var = tk.StringVar()
 director_var = tk.StringVar()
 
+
 # Defining a function that will
 # get the name and director and
 # print them on the screen
@@ -20,14 +21,17 @@ def submit():
     name_var.set("")
     director_var.set("")
 
+
 # Widgets
 movie_label = tk.Label(window, text='Movie', font=('calibre', 10, 'bold'))
 movie_entry = tk.Entry(window, textvariable=name_var, font=('calibre', 10, 'normal'))
 director_label = tk.Label(window, text='Director', font=('calibre', 10, 'bold'))
 director_entry = tk.Entry(window, textvariable=director_var, font=('calibre', 10, 'normal'))
 sub_btn = tk.Button(window, text='Submit', command=submit)
-
 content = tk.Label(window, text='Content', font=('calibre', 10, 'bold'))
+info_listbox = tk.Listbox(window)
+info_listbox.pack()
+
 
 # Grid placement
 movie_label.grid(row=0, column=0)
@@ -36,6 +40,12 @@ director_label.grid(row=1, column=0)
 director_entry.grid(row=1, column=1)
 sub_btn.grid(row=2, column=1)
 content.grid(row=4, column=0)
+sub_btn.pack(side = BOTTOM)
+info_listbox.insert(tk.END, 'name')
+info_listbox.insert(tk.END, 'director')
+
+customer_textbox = tk.Text(window, height=10, width=40)
+customer_textbox.pack()
 
 # Call mainloop
 window.mainloop()
